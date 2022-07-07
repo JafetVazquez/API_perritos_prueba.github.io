6bef97fe31e77dd1f886694ae6118a2e8a923cb8
Jafet Vazquez <jetvax.jv@gmail.com>
2022-07-06 23:02:44 -0500

    cambio en js

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ console.log('IDK')
 
 const URL = 'https://api.thedogapi.com/v1/images/search';
 
+
+//al abrir la página
+fetch(URL)
+    .then(result => result.json())
+    .then(data =>{
+        const img = document.querySelector('img');
+
+        img.src = data[0].url;
+    });
+
+
+
+//al recargar con el botón
 function recargar(){
     fetch(URL)
     .then(result => result.json())
